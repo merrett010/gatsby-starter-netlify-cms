3ae2d1b8ac5e9ae3bace1c9cec51483cd752ecb6
Lee Graham-Merrett <lee.grahammerrett@sky.uk>
2021-07-21 22:50:33 +0100

    Image attachment

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -11,6 +11,7 @@ export default class BlogIndexPage extends React.Component {
           className="full-width-image-container margin-top-0"
           style={{
             backgroundImage: `url('/img/trip-updates.jpeg')`,
+            backgroundAttachment: `fixed`,
           }}
         >
           <h1
